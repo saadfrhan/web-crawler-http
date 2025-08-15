a58f48735b3647a08bdf1577131acc63517dba67
saadfrhan <saadfarhan347@gmail.com>
2025-08-15 23:36:06 +0500

    created normalizeURL with tests

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
-console.log("Hello, world!");
+export function normalizeURL(urlString: string) {
+    const urlObj = new URL(urlString);
+    const hostPath = `${urlObj.hostname}${urlObj.pathname}`;
+    if (hostPath.length > 0 && hostPath.slice(-1) === "/") {
+        return hostPath.slice(0, -1);
+    }
+    return hostPath;
+}
